2ecf9379124c6633073fdaa3006082d7ac09e2d9
neemadeshwal <neemadeshwal.py@gmail.com>
2026-09-01 01:38:24 +0530

    Revert "feat: add conversation:join WebSocket event"
    This reverts commit d5320da986a9621ad060bcc1aeec0beeae5c931a.

--- api-gateway/src/module/chat/chat.module.ts
+++ api-gateway/src/module/chat/chat.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { AuthModule } from '../auth/auth.module';
-import { ConversationsModule } from '../conversations/conversations.module';
 
 // Note - we always import the entire module and not just few parts of it . 
 @Module({
-imports:[AuthModule,ConversationsModule],
+imports:[AuthModule],
   providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
